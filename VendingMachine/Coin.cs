b903da4e82287441455028694244f09592fc199c
HLind <hunterslind@gmail.com>
2018-04-30 18:15:08 -0500

    Added a penny value to the coin class.

--- VendingMachine/Coin.cs
+++ VendingMachine/Coin.cs
@@ -8,7 +8,10 @@ namespace VendingMachineKata
 {
     class Coin
     {
-        
+        /// <summary>
+        /// The following Tuple values are the diameter (cm) and weight (g) of known coins, stored respectively.
+        /// </summary>
+        public static readonly Tuple<double, double> PENNY = new Tuple<double, double>(19, 2.5);
         
     }
 }
