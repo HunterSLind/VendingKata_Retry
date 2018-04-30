738f7abf399abb59598109005f81288f92ca1af2
HLind <hunterslind@gmail.com>
2018-04-30 17:03:56 -0500

    Added some preliminary passing test logic. Won't build. Need to remove a dependency (should have just been added as a reference, not a project dependency).

--- VendingTests/VendingTests.cs
+++ VendingTests/VendingTests.cs
@@ -7,34 +7,41 @@ namespace VendingTests
     [TestClass]
     public class VendingTests
     {
+        VendingMachine.VendingMachine thisMachine;
+
+        [TestInitialize]
+        public void testInit()
+        {
+            thisMachine = new VendingMachine.VendingMachine();
+        }
         [TestMethod]
         public void DeclineCoin_Penny()
         {
-            
+            Assert.AreEqual(1, thisMachine.AcceptCoin());
         }
 
         [TestMethod]
         public void AcceptCoin_Nickel()
         {
-
+            Assert.AreEqual(1, thisMachine.AcceptCoin());
         }
 
         [TestMethod]
         public void AcceptCoin_Dime()
         {
-
+            Assert.AreEqual(1, thisMachine.AcceptCoin());
         }
 
         [TestMethod]
         public void AcceptCoin_Quarter()
         {
-            
+            Assert.AreEqual(1, thisMachine.AcceptCoin());
         }
 
         [TestMethod]
         public void DeclineCoin_BadCoin()
         {
-
+            Assert.AreEqual(1, thisMachine.AcceptCoin());
         }
     }
 }
