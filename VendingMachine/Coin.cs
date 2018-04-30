af7177bc1b85cd2912e8c0cf4a8351736370a087
HLind <hunterslind@gmail.com>
2018-04-30 18:28:39 -0500

    Added VALIDCOINDICTIONARY as private variable for class Coin.cs. This will house the valid coins information so that we can more easily check for valid coins. ValidCoin_Penny -- RED

--- VendingMachine/Coin.cs
+++ VendingMachine/Coin.cs
@@ -12,7 +12,11 @@ namespace VendingMachineKata
         /// The following Tuple values are the diameter (cm) and weight (g) of known coins, stored respectively.
         /// </summary>
         public static readonly Tuple<double, double> PENNY = new Tuple<double, double>(19, 2.5);
-        
+
+        private static readonly Dictionary<Tuple<double, double>, int> VALIDCOINDICTIONARY = new Dictionary<Tuple<double, double>, int>()
+        {
+        };
+
         public static bool isValidCoin(double diameter, double weight)
         {
             return true;
