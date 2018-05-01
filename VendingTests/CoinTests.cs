27d12f34cd8f928786d63f811284d1ec9f95839c
HLind <hunterslind@gmail.com>
2018-05-01 18:02:56 -0500

    Added comments to CoinTests

--- VendingTests/CoinTests.cs
+++ VendingTests/CoinTests.cs
@@ -18,24 +18,36 @@ namespace VendingTests
             testCoin = new Coin();
         }
 
+        /// <summary>
+        /// Validates that the value of a penny should be 0 since it's an invalid coin.
+        /// </summary>
         [TestMethod]
         public void ValidCoin_Penny()
         {
             Assert.AreEqual(0, Coin.CoinValue(Coin.PENNY.Item1, Coin.PENNY.Item2));
         }
 
+        /// <summary>
+        /// Validates that the value of a Nickels dimensions should be 5.
+        /// </summary>
         [TestMethod]
         public void ValidCoin_Nickel()
         {
             Assert.AreEqual(5, Coin.CoinValue(Coin.NICKEL.Item1, Coin.NICKEL.Item2));
         }
 
+        /// <summary>
+        /// Validates that the value of a Dimes dimensions should be 10.
+        /// </summary>
         [TestMethod]
         public void ValidCoin_Dime()
         {
             Assert.AreEqual(10, Coin.CoinValue(Coin.DIME.Item1, Coin.DIME.Item2));
         }
 
+        /// <summary>
+        /// Validates that the value of a Quarters dimensions should be 25.
+        /// </summary>
         [TestMethod]
         public void ValidCoin_Quarter()
         {
