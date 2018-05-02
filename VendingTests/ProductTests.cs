22c7d0e992ebbdd25d36a18d25b0ca63b356854b
HLind <hunterslind@gmail.com>
2018-05-01 19:17:59 -0500

    Added some failing logic for SelectProduct_Cola() SelectProduct_Cola -- RED

--- VendingTests/ProductTests.cs
+++ VendingTests/ProductTests.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachineKata;
 
 namespace VendingTests
 {
     [TestClass]
     public class ProductTests
     {
+        Product thisInventory;
+        /// <summary>
+        /// Test initialization: Creates a clean Product Object
+        /// </summary>
+        [TestInitialize]
+        public void testInit()
+        {
+            thisInventory = new Product();
+        }
+
         [TestMethod]
         public void SelectProduct_Cola()
         {
+            Assert.AreEqual(0, thisInventory);
         }
     }
 }
